ee24ff8ec1f04dfe50b5eb766efdf4ccba528ca4
Dragontail01 <146441768+Jused01@users.noreply.github.com>
2025-01-14 09:04:39 +0100

    x

--- Pokedex/pokeapp/views.py
+++ Pokedex/pokeapp/views.py
@@ -1,10 +1,11 @@
 from django.core.cache import cache
 import requests
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Pokemon
 def pokedex(request):
     search_query = request.GET.get('search', '').lower()
     pokemons = cache.get('translated_pokemons')
+    team = request.session.get('team', [])
 
     if not pokemons:
         api_url = "https://pokeapi.co/api/v2/pokemon?limit=150"
@@ -37,10 +38,39 @@ def pokedex(request):
     else:
         translated_pokemons = pokemons
 
+    # Ajouter un Pokémon à l'équipe
+    if 'add' in request.GET:
+        pokemon_name = request.GET.get('add')
+        if pokemon_name not in team and len(team) < 7:
+            team.append(pokemon_name)
+        request.session['team'] = team
+
+    # Retirer un Pokémon de l'équipe
+    if 'remove' in request.GET:
+        pokemon_name = request.GET.get('remove')
+        if pokemon_name in team:
+            team.remove(pokemon_name)
+        request.session['team'] = team
+
     # Filtrer les résultats
     if search_query:
         translated_pokemons = [
             p for p in translated_pokemons if search_query in p['name'].lower()
         ]
 
-    return render(request, 'pokedex.html', {'pokemons': translated_pokemons})
+    return render(request, 'pokedex.html', {
+        'pokemons': translated_pokemons,
+        'team': team,
+    })
+
+
+def team_view(request):
+    pokemons = cache.get('translated_pokemons', [])
+    team_names = request.session.get('team', [])
+
+    # Récupérer les détails des Pokémon de l'équipe
+    team = [
+        pokemon for pokemon in pokemons if pokemon['name'] in team_names
+    ]
+
+    return render(request, 'team.html', {'team': team})
