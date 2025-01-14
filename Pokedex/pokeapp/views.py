402e10d8ba15d2fa60d68895b377782eb54b00ed
Dragontail01 <146441768+Jused01@users.noreply.github.com>
2025-01-14 08:52:18 +0100

    pokedex,teams,views,url

--- Pokedex/pokeapp/views.py
+++ Pokedex/pokeapp/views.py
@@ -2,9 +2,10 @@ from django.core.cache import cache
 import requests
 from django.shortcuts import render
 from .models import Pokemon
-
 def pokedex(request):
+    search_query = request.GET.get('search', '').lower()
     pokemons = cache.get('translated_pokemons')
+
     if not pokemons:
         api_url = "https://pokeapi.co/api/v2/pokemon?limit=150"
         response = requests.get(api_url)
@@ -13,22 +14,22 @@ def pokedex(request):
             translated_pokemons = []
 
             for pokemon in pokemons:
-                species_url = f"https://pokeapi.co/api/v2/pokemon-species/{pokemon['name']}"
-                sprite_url = f"https://pokeapi.co/api/v2/pokemon/{pokemon['name']}"
+                name = pokemon['name']
+                species_url = f"https://pokeapi.co/api/v2/pokemon-species/{name}"
+                sprite_url = f"https://pokeapi.co/api/v2/pokemon/{name}"
                 species_response = requests.get(species_url)
                 sprite_response = requests.get(sprite_url)
-                if species_response.status_code == 200:
+
+                if species_response.status_code == 200 and sprite_response.status_code == 200:
                     species_data = species_response.json()
                     sprite_data = sprite_response.json()
-                    image_url = sprite_data.get('sprites', {}).get('front_default', None)
                     french_name = next(
-                        (name['name'] for name in species_data['names'] if name['language']['name'] == 'fr'),
-                        pokemon['name']
+                        (n['name'] for n in species_data['names'] if n['language']['name'] == 'fr'),
+                        name
                     )
                     translated_pokemons.append({
                         'name': french_name,
-                        'url': pokemon['url'],
-                        'image_url': image_url 
+                        'image_url': sprite_data.get('sprites', {}).get('front_default', None)
                     })
             cache.set('translated_pokemons', translated_pokemons, timeout=3600)
         else:
@@ -36,7 +37,10 @@ def pokedex(request):
     else:
         translated_pokemons = pokemons
 
-    context = {
-        'pokemons': translated_pokemons,
-    }
-    return render(request, 'pokedex.html', context)
+    # Filtrer les résultats
+    if search_query:
+        translated_pokemons = [
+            p for p in translated_pokemons if search_query in p['name'].lower()
+        ]
+
+    return render(request, 'pokedex.html', {'pokemons': translated_pokemons})
